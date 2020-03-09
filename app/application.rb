aea141749e0e98c337b38e3c396ad12e95b4a8a8
Stela Capsa <stela.capsa@gmail.com>
2020-03-09 14:59:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,15 +27,13 @@ class Application
       end 
         
     
-      
-      
     if req.path.match(/add/)
 			item = req.params["item"]
 	 
 			if 	@@items.include?(item)
 				@@cart << item
 				resp.write "added #{item}"
-			else
+			 else
 				resp.write "We don't have that item"
 			end
 		end 
