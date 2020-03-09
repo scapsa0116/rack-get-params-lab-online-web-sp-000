0adf7d2fb282768bed905dfe6d22471cd05d32b1
Stela Capsa <stela.capsa@gmail.com>
2020-03-09 14:49:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
   @@cart = []
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Request.new(env)
+    # req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       @@items.each do |item|
