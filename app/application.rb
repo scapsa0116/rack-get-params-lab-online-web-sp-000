c0a2dc92ff090beb8497ac24eb45519f881cea82
Stela Capsa <stela.capsa@gmail.com>
2020-03-09 14:34:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
       end 
       
       
-      if req.path.match(/add/)
+      else req.path.match(/add/)
       search_term = req.params["item"]
       if @@items.include?(search_term)
         @@cart << search_term
