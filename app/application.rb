0434ed86dc39f553a14bfd44b7d55e6891778c65
Stela Capsa <stela.capsa@gmail.com>
2020-03-09 14:29:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -39,11 +39,7 @@ class Application
       else
         resp.write "We don't have that item"
       end
-         else
-      resp.write "Path Not Found"	
+      
+     
   end
-  
-  
-  
-  
 end
